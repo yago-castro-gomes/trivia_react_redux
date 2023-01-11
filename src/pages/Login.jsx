119fbe37c97d4d673cf8b2de04da71d5805bca19
AndersonCR <anderson.c.rodrigues@hotmail.com>
2023-01-11 15:41:51 -0300

    [feat] adiciona validação do botão Play

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,62 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Login extends Component {
+  state = {
+    name: '',
+    email: '',
+    isDisable: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value }, this.handlevalid);
+  };
+
+  handlevalid = () => {
+    const { name, email } = this.state;
+    const MIN = 3;
+    const nameValid = name.length >= MIN;
+    const regexMail = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+    const emailValid = regexMail.test(email);
+
+    this.setState({ isDisable: !(nameValid && emailValid) });
+  };
+
   render() {
+    const { name, email, isDisable } = this.state;
     return (
-      <div>login</div>
+      <form>
+        <label htmlFor="name">
+          Nome
+          <input
+            type="text"
+            name="name"
+            id="name"
+            data-testid="input-player-name"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="email">
+          E-mail
+          <input
+            type="text"
+            name="email"
+            id="email"
+            data-testid="input-gravatar-email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="btn-play"
+          disabled={ isDisable }
+        >
+          Play
+
+        </button>
+      </form>
     );
   }
 }
