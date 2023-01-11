ad9da882ea7cc544d2c24c0f15a94c87d8de3de7
Yago Castro <yago.fanstour@gmail.com>
2023-01-11 18:31:03 -0300

    [feat]: cria a página game, cria actions, reduce do login,gera imagem do gravatar, exibe nome e score na tela.

--- src/redux/reduce/index.js
+++ src/redux/reduce/index.js
@@ -1,3 +1,5 @@
+import { GRAVATAREMAIL, NAME } from '../action';
+
 const INITIAL_STATE = {
   name: '',
   assertions: '',
@@ -7,6 +9,16 @@ const INITIAL_STATE = {
 
 const reduce = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case GRAVATAREMAIL:
+    return {
+      ...state,
+      gravatarEmail: action.payload,
+    };
+  case NAME:
+    return {
+      ...state,
+      name: action.payload,
+    };
   default: {
     return state;
   }
