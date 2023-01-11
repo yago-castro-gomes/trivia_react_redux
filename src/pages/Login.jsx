d18fe4ec9205ddaabe3530adca0c90875b8fab4b
Ikarosv <ikarovieiraa@gmail.com>
2023-01-11 17:12:15 -0300

    refactor: muda import

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { fetchTokenTrivia } from '../services/apiTrivia';
 import { Link } from 'react-router-dom';
+import { fetchTokenTrivia } from '../services/apiTrivia';
 
 class Login extends Component {
   state = {
