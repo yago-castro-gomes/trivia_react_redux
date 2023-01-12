72e3e80d65a2d90b7580bf9317140b29e9516db8
AndersonCR <anderson.c.rodrigues@hotmail.com>
2023-01-12 09:06:50 -0300

    [refactor] game: resposta incorreta começam com indice 0

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -4,7 +4,8 @@ import P from 'prop-types';
 import fetchAnswerTrivia from '../services/apiAnswer';
 import Header from '../components/Header';
 
-// let countIndex = -1;
+const MINUS = -1;
+let countIndex = MINUS;
 
 class Game extends Component {
   state = {
@@ -34,20 +35,17 @@ class Game extends Component {
     const arr2 = [];
 
     arr.forEach((element) => {
-      // arr2.push(arr[Math.floor(Math.random() * arr.length)]);
-
-      // const index = arr.indexOf(arr2[arr2.length - 1]);
       arr2.splice(Math.floor(Math.random() * arr.length), 0, element);
     });
 
     this.setState({ rndAnswer: arr2 });
   };
 
-  // handleNumber = () => {
-  //   const THREE = 3;
-  //   countIndex = countIndex > THREE ? 0 : countIndex + 1;
-  //   return countIndex;
-  // };
+  handleNumber = () => {
+    const THREE = 3;
+    countIndex = countIndex > THREE ? 0 : countIndex + 1;
+    return countIndex;
+  };
 
   render() {
     const { rndAnswer, answer, answerNumber } = this.state;
@@ -67,7 +65,7 @@ class Game extends Component {
                   type="button"
                   data-testid={ item === answer[answerNumber].correct_answer
                     ? 'correct-answer'
-                    : `wrong-answer-${index}` }
+                    : `wrong-answer-${this.handleNumber()}` }
                 >
                   {item}
                 </button>
