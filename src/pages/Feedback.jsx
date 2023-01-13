a5eab48c160550000af6c80e1da397098c7356a4
Danilo Argolo <dan_argolo@hotmail.com>
2023-01-13 14:16:48 -0300

    [Feat] Implementa o botão Ranking e botão Inicio

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Feedback extends Component {
   render() {
     return (
-      <div data-testid="feedback-text">Feedback</div>
+      <>
+        <p data-testid="feedback-text">Feedback</p>
+        <Link to="/ranking">
+          <button
+            type="button"
+            data-testid="btn-ranking"
+          >
+            Ranking
+          </button>
+        </Link>
+      </>
     );
   }
 }
