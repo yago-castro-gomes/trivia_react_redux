679fb850ee8b0ca9c8bbebb968f2c4a45924ea7d
AndersonCR <anderson.c.rodrigues@hotmail.com>
2023-01-13 14:53:50 -0300

    [feat] adiciona action para somar assertions ao estado global

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,4 @@
-import { GRAVATAREMAIL, NAME, SUM_SCORE } from '../action';
+import { ADD_ASSETTIONS, GRAVATAREMAIL, NAME, SUM_SCORE } from '../action';
 
 const INITIAL_STATE = {
   player: {
@@ -25,14 +25,20 @@ const reduce = (state = INITIAL_STATE, action) => {
         ...state.player, name: action.payload,
       },
     };
-  case SUM_SCORE: {
+  case SUM_SCORE:
     return {
       ...state,
       player: {
         ...state.player, score: state.player.score + action.payload,
       },
     };
-  }
+  case ADD_ASSETTIONS:
+    return {
+      ...state,
+      player: {
+        ...state.player, assertions: state.player.assertions + 1,
+      },
+    };
   default: {
     return state;
   }
